7fc6ea2ae2ae3a9663d6bde06798aeaed54d444f
JOSUE RIVERA <josuerivera@JOSUEs-Air.hitronhub.home>
2019-10-18 22:19:09 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var userAge = prompt("how old are you?");
+
+if(userAge >=21) {
+	alert("you may enter");
+}else if (userAge >=18) {
+	alert("you may enter, but you cant drink");
+}else {
+	alert("you many not pass")
+}
